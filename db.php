ed0389e15997bbb7061103bb2bab7f6e02be151e
Nisha Dhull <nishadhull388@gmail.com>
2026-05-05 14:09:38 +0530

    my codes upload

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
-$conn = mysqli_connect("YOUR_HOST", "YOUR_USERNAME", "YOUR_PASSWORD", "YOUR_DATABASE");
-
+$conn = mysqli_connect("sql100.infinityfree.com", "if0_41676805", "e0lxRpN4Ocvj3", "if0_41676805_EduNexus");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
-if($username == "YOUR_USERNAME" && $password == "YOUR_PASSWORD"){
+if($username == "admin" && $password == "1475"){
     $_SESSION['user'] = $username;
     $_SESSION['role'] = 'admin';
     header("Location: dashboard.php");
